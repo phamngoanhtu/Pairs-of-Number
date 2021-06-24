ae7acfebfe30ca772af1e30320b6e62d86eae001
phamngoanhtu <82858239+phamngoanhtu@users.noreply.github.com>
2021-06-24 14:06:04 +0700

    Add files via upload

--- Pairsofnumber.py
+++ Pairsofnumber.py
@@ -7,10 +7,10 @@ n = int (input())
 for i in range(n):
     ar.append(sys.stdin.readline())
 arset = set(ar)
-
+#For more info about why set: https://www.w3schools.com/python/python_sets.asp
 for i in arset:
     cnt_ele = ar.count(i)
     if cnt_ele >= 2:
-        pairs += ((cnt_ele)*(cnt_ele - 1)/2)
+        pairs += ((cnt_ele)*(cnt_ele - 1)/2) #Formula: n*(n-1)/2
 print(int(pairs))
 
